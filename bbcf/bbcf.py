01bce2c594d8bb60570e7951df079ddee55ce0a4
Mojotroll <Mojotroll@users.noreply.github.com>
2017-10-24 00:39:15 -0400

    BBCF v1.5.3.2.1
    Arakune Updates and typo fix on equal zero

--- bbcf/bbcf.py
+++ bbcf/bbcf.py
@@ -19,7 +19,7 @@ class bbcf:
 
     def __init__(self, bot):
         self.bot = bot
-        self.version = "1.5.3.2"
+        self.version = "1.5.3.2.1"
 
     @commands.group(pass_context=True)
     async def am(self, ctx):
@@ -57776,7 +57776,7 @@ class bbcf:
             mpA = "```" + "*" + mp2[0] + "\n" + "*" + mp2[1] + "\n" + "*" + mp1[0] + "\n" + "*" + mp1[1] + "\n" + "*" + mp2[2] + "\n" + "*" + mp1[2] + "\n" + "*" + mp1[3] + "\n" + "*" + mp1[4] + "\n" + "*" + mp1[5] + "\n" + "*" + mp1[6] + "\n" + "*" + mp1[7] + "\n" + "*" + mp1[8] + "\n" + "*" + mp1[9] + "\n" + "*" + mp1[10] + "\n" + "*" + mp2[3] + "```" 
 			
             embed = discord.Embed()
-            embed.title = "Bang's Void Tempest Kick"
+            embed.title = "Equals Zero"
             embed.set_image(url='http://www.dustloop.com/wiki/images/c/c0/BBCS_Arakune_EqualsZero.png')
             embed.add_field(name="Frame Data", value=tab)
             embed.add_field(name="Attack Props:", value=mpA, inline='true')		
@@ -58707,7 +58707,7 @@ class bbcf:
        
             tab = tabulate([["Version", vrs], ["Damage", Data[0]], ["Cancel", Data[1]], ["Guard", Data[2]], ["Startup", Data[3]], ["Active", Data[4]], ["Recovery", Data[5]], ["Frame Adv.", Data[6]], ["Attribute", Data[7]]], tablefmt='simple', stralign='left')
             tab1 = tabulate([["Version", vrs1], ["Damage", Data1[0]], ["Cancel", Data1[1]], ["Guard", Data1[2]], ["Startup", Data1[3]], ["Active", Data1[4]], ["Recovery", Data1[5]], ["Frame Adv.", Data1[6]], ["Attribute", Data1[7]]], tablefmt='simple', stralign='left')
-            mpA = "```" + "*" + mp1[0] + "\n" + "*" + mp1[1] + "\n" + "*" + mp1[2] + "\n" + "*" + mp1[3] + "\n" + "*" + mp1[4] + "\n" + "*" + mp2 + "```"
+            mpA = "```" + "*" + mp1[0] + "\n" + "*" + mp1[1] + "\n" + "*" + mp1[2] + "\n" + "*" + mp1[3] + "\n" + "*" + mp1[4] + "\n" + "*" + mp1[5] + "\n" + "*" + mp2 + "```"
 			
             embed = discord.Embed()
             embed.title = "n Factorial"
