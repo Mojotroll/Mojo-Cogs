63ad69c0ba3c9b078374c5da0d69e7587c2a4fdb
Mojotroll <Mojotroll@users.noreply.github.com>
2021-06-06 09:37:44 -0400

    Add files via upload

--- Diomedes/diomedes.py
+++ Diomedes/diomedes.py
@@ -7,6 +7,7 @@ class diomedes(commands.Cog):
         self.bot = bot
         self.quotes = ["No foe shall see my mercy", "You are ready for war now", "You are ready to excel now", "You are well suited now for victory", "Be Stregnthened and be ready", "Might and Honor, Glory and Victory", "Steady yourself brother", "Rise renewed for battle", "You reek of fear and frailty", "You tremble because you are weak", "Your Weakness shows", "See what fight you have in you now", "Brother, I am Pinned Here!", "Orders? Moving Now", "Orks", "Death to his enemies all", "That noise cannot defeat me", "Ah, a warp spider", "ITS THE BANEBLADE!", "You dare?! YOU DARE!?", "Departing Now", "Moving Now", "Retreat now for victory later"]
 
+    @commands.Cog.listener()
     async def diomedes(self, message):
         """Captain Diomedes with another banger"""
         if message.author.id == self.bot.user.id:
