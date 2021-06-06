8710a87b2d1155e613f90bb2764cac179f50eb05
Mojotroll <Mojotroll@users.noreply.github.com>
2021-06-06 09:23:10 -0400

    Add files via upload

--- Diomedes/__init__.py
+++ Diomedes/__init__.py
@@ -1,8 +1,5 @@
 from .diomedes import diomedes
 
-self.bot = bot
-self.quotes = ["No foe shall see my mercy", "You are ready for war now", "You are ready to excel now", "You are well suited now for victory", "Be Stregnthened and be ready", "Might and Honor, Glory and Victory", "Steady yourself brother", "Rise renewed for battle", "You reek of fear and frailty", "You tremble because you are weak", "Your Weakness shows", "See what fight you have in you now", "Brother, I am Pinned Here!", "Orders? Moving Now", "Orks", "Death to his enemies all", "That noise cannot defeat me", "Ah, a warp spider", "ITS THE BANEBLADE!", "You dare?! YOU DARE!?", "Departing Now", "Moving Now", "Retreat now for victory later"]
-
 def setup(bot):
     n = diomedes(bot)
     bot.add_listener(n.diomedes, "on_message")
--- Diomedes/diomedes.py
+++ Diomedes/diomedes.py
@@ -3,6 +3,9 @@ import random
 
 class diomedes(commands.Cog):
     """You have Captain Diomedes."""
+    def __init__(self, bot):
+        self.bot = bot
+        self.quotes = ["No foe shall see my mercy", "You are ready for war now", "You are ready to excel now", "You are well suited now for victory", "Be Stregnthened and be ready", "Might and Honor, Glory and Victory", "Steady yourself brother", "Rise renewed for battle", "You reek of fear and frailty", "You tremble because you are weak", "Your Weakness shows", "See what fight you have in you now", "Brother, I am Pinned Here!", "Orders? Moving Now", "Orks", "Death to his enemies all", "That noise cannot defeat me", "Ah, a warp spider", "ITS THE BANEBLADE!", "You dare?! YOU DARE!?", "Departing Now", "Moving Now", "Retreat now for victory later"]
 
     async def diomedes(self, message):
         """Captain Diomedes with another banger"""
