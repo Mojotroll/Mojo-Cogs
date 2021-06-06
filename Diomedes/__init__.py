d5e4c96c48730d461109213849f5c3d9975ce12b
Mojotroll <Mojotroll@users.noreply.github.com>
2021-06-06 09:50:10 -0400

    Add files via upload

--- Diomedes/__init__.py
+++ Diomedes/__init__.py
@@ -2,5 +2,5 @@ from .diomedes import diomedes
 
 def setup(bot):
     n = diomedes(bot)
-    bot.add_listener(n.diomedes, "on_message")
+    bot.add_listener(n.on_message, "on_message")
     bot.add_cog(n)
