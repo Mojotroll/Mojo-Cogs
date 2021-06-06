9ac422a7180c72d07735d2fe219812d37edea02d
Mojotroll <Mojotroll@users.noreply.github.com>
2021-06-06 09:48:45 -0400

    Add files via upload

--- Diomedes/diomedes.py
+++ Diomedes/diomedes.py
@@ -8,11 +8,16 @@ class diomedes(commands.Cog):
         self.quotes = ["No foe shall see my mercy", "You are ready for war now", "You are ready to excel now", "You are well suited now for victory", "Be Stregnthened and be ready", "Might and Honor, Glory and Victory", "Steady yourself brother", "Rise renewed for battle", "You reek of fear and frailty", "You tremble because you are weak", "Your Weakness shows", "See what fight you have in you now", "Brother, I am Pinned Here!", "Orders? Moving Now", "Orks", "Death to his enemies all", "That noise cannot defeat me", "Ah, a warp spider", "ITS THE BANEBLADE!", "You dare?! YOU DARE!?", "Departing Now", "Moving Now", "Retreat now for victory later"]
 
     @commands.Cog.listener()
-    async def diomedes(self, message):
+    async def on_message(self, *args):
         """Captain Diomedes with another banger"""
+        message = args[-1]
+        assert isinstance(message, discord.Message)
         if message.author.id == self.bot.user.id:
+            return
+        assert isinstance (message.channel, discord.TextChannel)
+        if not message.channel.permissions_for(message.guild.me).manage_messages:
             return
         for x in message.content.split():
             if x.lower() == "captain diomedes":
                 warpSpider = random.choice(self.quotes)  
-                await self.bot.send(warpSpider)
+                await message.send(warpSpider)
