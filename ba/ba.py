92c6dac9ed1f81e79ecbe3e788292087f25d8522
Mojotroll <Mojotroll@users.noreply.github.com>
2022-03-26 12:56:20 -0400

    Case correction and now have coffee

---
+++ ba/ba.py
@@ -0,0 +1,21 @@
+import discord
+from redbot.core import checks, Config, commands, bot
+
+class ba(commands.Cog):
+    """For the Suave Gentlemen of the Abyss"""
+    def __init__(self, bot):
+        self.bot = bot
+        self.image = "images/ltd.png"
+
+    
+    
+    @commands.command()
+    async def platinspiration(self, ctx):
+        """Survive Plat One with Euphoric Inspiration"""
+
+        await self.bot.send(file=discord.File(self.image))
+
+
+
+
+    
